90d696d00e63855d439c425dad780ff32dceef27
Vu Huy <vuhuycto@gmail.com>
2021-01-09 21:34:50 +0700

    Updates

---
+++ precache-manifest.7197a9a463a1c7f5a2f5ec323755171e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1b287d806709de4a346cc5e914991ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81946eea416f3da0c35d",
+    "url": "/static/css/main.4cff756b.chunk.css"
+  },
+  {
+    "revision": "9b4b15461ab82671fd96",
+    "url": "/static/js/2.89e2f2f4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.89e2f2f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81946eea416f3da0c35d",
+    "url": "/static/js/main.6abeba40.chunk.js"
+  },
+  {
+    "revision": "2b732d0f86f82bed2045",
+    "url": "/static/js/runtime-main.8a45d930.js"
+  },
+  {
+    "revision": "61f3a8a387a64abfc0535cd0813cd71a",
+    "url": "/static/media/linea-basic-10.61f3a8a3.woff"
+  },
+  {
+    "revision": "ce776cff0588d8e8385c75a3c8958f0d",
+    "url": "/static/media/linea-basic-10.ce776cff.svg"
+  },
+  {
+    "revision": "e4de528c00da0c081906c3caea289f0f",
+    "url": "/static/media/linea-basic-10.e4de528c.eot"
+  },
+  {
+    "revision": "e6763f008205c55db623cdf68bbd581f",
+    "url": "/static/media/linea-basic-10.e6763f00.ttf"
+  }
+]);
